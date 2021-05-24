c85b2d957eec105b17d598ed4e1184e019bcea9a
clement <rezperez@gmail.com>
2021-05-24 09:46:44 -0700

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -15,9 +15,26 @@ const getRandomMapping = () => {
   return map;
 };
 
+const offsetMap = (offset) => {
+  let i = 0;
+  const map = {};
+  const a = "a".charCodeAt(0);
+  while (i < 26) {
+    const charCode = (i + offset) % 26;
+    const char = String.fromCharCode(a + charCode);
+    map[String.fromCharCode(a + i)] = char;
+    i++;
+  }
+  return map;
+};
+
 cipherString = (string) => {
   const map = getRandomMapping();
   return { map, cipher: decypher(string, map) };
 };
 
+cipherCeasarString = (string) => {
+  const map = offsetMap(1);
+  return { map, cipher: decypher(string, map) };
+};
 module.exports = cipherString;
